112826983e3f2b4ef3d56723efde7141a543b20c
odisei369 <petrenko.ilia@gmail.com>
2017-10-15 14:28:19 +0200

    fix

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -7,8 +7,8 @@ class CardComponent extends Component {
     super();
     this.state = {
       events: [],
-      description: 'Phasellus quis turpis eu sapien suscipit mollis. Vivamus ornare laoreet felis, tempor suscipit leo',
-      title: 'bitwa wartszawa',
+      description: '',
+      title: '',
       open: false,
       imgSrc: '../assets/img/hotel.JPG',
     };
@@ -32,17 +32,17 @@ class CardComponent extends Component {
   }
 
   render() {
-    if (!this.state.events.length) {
+    if (this.state.events.length) {
       return <div>
         <div className="row">
           <div className="col-md-12">
             <div className="card">
               <div className="card__title">
-                <h1>{this.state.title}</h1>
+                <h1>{this.state.events[0].name}</h1>
               </div>
               <div className="card__body">
                 <article>
-                  <p>{this.state.description}</p>
+                  <p>{this.state.events[0].content}</p>
                   <Panel collapsible expanded={this.state.open}>
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
                     Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
@@ -66,10 +66,8 @@ class CardComponent extends Component {
           <div className="col-md-12">
             <div className="card">
               <div className="card__title">
-                <h1>{this.state.events[0].name}</h1>
               </div>
               <div className="card__body">
-                <h2>{this.state.events[0].content}</h2>
               </div>
             </div>
           </div>
