282ace7eb25d543965c2c91deaa9f3a85dc99529
odisei369 <petrenko.ilia@gmail.com>
2017-10-15 12:42:16 +0200

    websocket working, add messagecomponent

--- src/Components/ChatComponent.js
+++ src/Components/ChatComponent.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+
+import MessageComponent from './MessageComponent';
 
 class ChatComponent extends Component {
   constructor() {
     super();
     this.state = {
       messages: [],
+      message: '',
+      name: ''
     };
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeMessage = this.handleChangeMessage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
   }
-  sendMessage(text) {
+  sendMessage(text, user) {
     clientInformation.message = text;
     clientInformation.date = new Date().toLocaleString();
-    //conn.send(JSON.stringify(clientInformation));
+    clientInformation.user= user;
+    this.conn.send(JSON.stringify(clientInformation));
   }
 
   componentWillMount() {
+
+    this.conn = new WebSocket('ws://localhost:8080');
+    this.conn.component = this;
+    this.conn.onopen = function(e) {
+      console.log("Connection established succesfully");
+    };
+
+    this.conn.onmessage = function (e) {
+      var data = JSON.parse(e.data);
+      this.component.setState({messages: this.component.state.messages.concat([{user: data.user, message: data.message, date: data.date}])});
+      console.log(this.component.state.messages);
+    };
+
+    this.conn.onerror = function (e) {
+      alert("Error: something went wrong with the socket.");
+      console.error(e);
+    };
 
 
     fetch(`${process.env.API_SERVER}/messages`)
@@ -23,38 +49,36 @@ class ChatComponent extends Component {
         .catch(error => {
           console.log(error)
         });
-    this.webSocket();
   }
-
-  webSocket = () => {
-    const conn = new WebSocket('ws://localhost:8080');
-
-    conn.onopen = function(e) {
-      console.log("Connection established succesfully");
-    };
-
-    conn.onmessage = function (e) {
-      var data = JSON.parse(e.data);
-      this.state.messages.push({user: data.username, message: data.message, date: data.date});
-    };
-
-    conn.onerror = function (e) {
-      alert("Error: something went wrong with the socket.");
-      console.error(e);
-    };
+  handleSubmit(event){
+    this.sendMessage(this.state.message, this.state.name);
+    this.setState({name: ''});
+    this.setState({message: ''});
+  }
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+  handleChangeMessage(event) {
+    this.setState({message: event.target.value});
   }
 
 
   render() {
+    const listMessages = this.state.messages.map((post) =>
+      <MessageComponent message={post.message} user={post.user} date={post.date} />);
+
     return <div>
-      <form>
+      <div className="container">
+        {listMessages}
+      </div>
+      <form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="email" className="form-control" id="name" placeholder="Jan Kowalski"/>
+          <input type="name" className="form-control" id="name" placeholder="Jan Kowalski" onChange={this.handleChangeName}/>
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea className="form-control" id="meassage" rows="3"></textarea>
+          <textarea className="form-control" id="meassage" rows="3" onChange={this.handleChangeMessage}></textarea>
         </div>
         <button type="submit" className="btn btn-primary">Send</button>
       </form>
