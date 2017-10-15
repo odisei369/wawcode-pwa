d7ef9e06dfabf7fceb693fc5be537d0b97630f9e
odisei369 <petrenko.ilia@gmail.com>
2017-10-15 09:59:47 +0200

    add form to chat

--- src/Components/ChatComponent.js
+++ src/Components/ChatComponent.js
@@ -4,7 +4,17 @@ import React, { Component } from 'react';
 class ChatComponent extends Component {
   render() {
     return <div className="container">
-      <h1>Here would be chat</h1>
+      <form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="email" class="form-control" id="name" placeholder="Jan Kowalski"/>
+        </div>
+        <div class="form-group">
+          <label for="message">Message</label>
+          <textarea class="form-control" id="meassage" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Send</button>
+      </form>
     </div>
   }
 }
