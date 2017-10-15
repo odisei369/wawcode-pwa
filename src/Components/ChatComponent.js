6d67b0d55146a1c597d0f78d6990d7f6b3248766
odisei369 <petrenko.ilia@gmail.com>
2017-10-15 10:06:23 +0200

    add env

--- src/Components/ChatComponent.js
+++ src/Components/ChatComponent.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class ChatComponent extends Component {
+  constructor () {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+  componentWillMount () {
+
+
+    axios.get('http://localhost:8000/month-day/1/1', { crossdomain: true })
+    //.then(response => response.json())
+    //.then(data => console.log(data))
+    // We set the latest prices in the state to the prices gotten from Cryptocurrency.
+    // this.setState({ name: response.name });
+    // this.setState({ content: response.content });
+    // this.setState({ year: response.year });
+
+    // Catch any error here
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+
   render() {
     return <div className="container">
       <form>
